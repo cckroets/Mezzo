8d4aab48e0d73d5acca72974f31c4ec70043dec7
Curtis Kroetsch <kurtis.kroetsch@gmail.com>
2015-02-01 22:40:51 -0500

    Added a drawer toggle to the actionbar to open and close the drawer.

--- app/src/main/java/cs446/mezzo/app/MainActivity.java
+++ app/src/main/java/cs446/mezzo/app/MainActivity.java
@@ -1,7 +1,9 @@
 package cs446.mezzo.app;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,12 +26,28 @@ public class MainActivity extends BaseMezzoActivity {
     @Inject
     MezzoPlayer mMezzoPlayer;
 
+    ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setSupportActionBar(mToolbar);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mNavDrawer, R.string.app_name, R.string.app_name);
+        mNavDrawer.setDrawerListener(mDrawerToggle);
     }
 
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +61,10 @@ public class MainActivity extends BaseMezzoActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         final int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
